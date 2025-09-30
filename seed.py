9d34aa5f2e590474d369039ea4d3351c8902f3ba
Annie <annie.batsaikhan87@gmail.com>
2025-09-30 09:36:51 +0900

    Add three high-coverage seeds

---
+++ seed.py
@@ -0,0 +1,4 @@
+
+SEED1 = '{"s":"\\u0041","arr":[null,true,false,-0.1,2e10,0],"o":{"k":"v","empty":{}}}'
+SEED2 = '"escaped \\"quote\\" \\\\ backslash \\u263A \\n \\t / \\b \\/"'
+SEED3 = '{"incomplete": [ "unterminated string, 123,, { "partial": tru'
